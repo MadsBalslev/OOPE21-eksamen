6def4ad4fa0731cba73fb8d3805b52149aa2d248
Mads Balslev <madspbalslev@gmail.com>
2021-10-30 12:34:09 +0200

    Add User-model

--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,30 @@
+using System;
+
 namespace Stregsystem.Models
 {
-    public class User
+  public class User : IComparable
+  {
+    public User(int id, string fName, string lName, string uName, string email, decimal balance)
     {
-        
+		this._id = id;
+		this.Firstname = fName;
+		this.Lastname = lName;
+		this.Username = uName;
+		this.Email = email;
+		this.Balance = balance;
     }
+    private int _id;
+    private string Firstname { get; set; }
+    private string Lastname { get; set; }
+    private string Username { get; set; }
+    private string Email { get; set; }
+    private decimal Balance { get; set; }
+
+    public override string ToString() => $"{Firstname} {Lastname} ({Email}";
+
+    public int CompareTo(object obj)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
