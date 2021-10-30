c35b166cf8a83e9b1895638764da2a9b2ae73c6d
Mads Balslev <madspbalslev@gmail.com>
2021-10-30 12:09:35 +0200

    Initial commit :tada:

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stregsystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
